2e3b39d180e55706a3a8cbfcc3b9ae980b2ee55a
xavipropiedades <xavipropiedades@gmail.com>
2026-06-24 17:48:14 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
-    const URL_PROPIEDADES = "https://docs.google.com/spreadsheets/d/1VctscCRyoQ-sdWa1vlGG0xsjjGY5Jznw6LaK20syz3g/export?format=csv&gid=0";
-    const URL_CONFIG = "https://docs.google.com/spreadsheets/d/1VctscCRyoQ-sdWa1vlGG0xsjjGY5Jznw6LaK20syz3g/export?format=csv&gid=563916861";
+    const URL_PROPIEDADES = "https://docs.google.com/spreadsheets/d/1lSO20Y4w3GLdJIl2Eayr8wuGcF_qi5qQ7Wkcwo63DSs/export?format=csv";
+    const URL_CONFIG = "https://docs.google.com/spreadsheets/d/1lSO20Y4w3GLdJIl2Eayr8wuGcF_qi5qQ7Wkcwo63DSs/export?format=csv&gid=1786999046";
 
     try {
         const [resProp, resConfig] = await Promise.all([
